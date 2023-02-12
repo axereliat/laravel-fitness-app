ce3d55663ffc3a97d4f11fe6a62c04d5dd349458
axereliat <axereliat@gmail.com>
2023-02-12 20:18:22 +0200

    Records filter

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -6,20 +6,28 @@ use App\Http\Requests\RecordPostRequest;
 use App\Models\DailyActivity;
 use App\Models\Record;
 use DB;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Request;
 
 class RecordController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $activities = DailyActivity::all();
+
         $records = DB::table('records')
             ->join('daily_activities', 'daily_activity_id', '=', 'daily_activities.id')
             ->select(['records.*', 'daily_activities.name as daily_activity_name'])
             ->where('user_id', '=', Auth::id())
             ->get();
 
-        return view('records.list-records', ['records' => $records]);
+        $activityId = $request->query('activity') ?? 0;
+
+        if ($activityId > 0) {
+            $records = $records->where('daily_activity_id', $activityId);
+        }
+
+        return view('records.list-records', ['records' => $records, 'activities' => $activities]);
     }
 
     public function create()
--- resources/views/records/list-records.blade.php
+++ resources/views/records/list-records.blade.php
@@ -10,6 +10,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <form>
+                        <label for="activity">Choose activity</label>
+                        <select name="activity" id="activity">
+                            <option value="0">
+                                All
+                            </option>
+                            @foreach($activities as $activity)
+                                <option value="{{ $activity->id }}"
+                                    {{ +request()->query->get('activity') === +$activity->id ? 'selected' : '' }}>
+                                    {{ $activity->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <x-primary-button type="submit">Filter</x-primary-button>
+                    </form>
+                    <br/>
                     @foreach($records as $record)
                         <div>
                             <h3>{{ $record->daily_activity_name }}</h3>
